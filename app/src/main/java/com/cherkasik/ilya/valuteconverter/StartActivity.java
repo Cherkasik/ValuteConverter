36b889ae800653b9861f55201cb285f138f34763
Cherkasik <ilyakopeysk@gmail.com>
2019-06-12 22:44:11 +0300

    added filling spinners and some TODO

--- app/src/main/java/com/cherkasik/ilya/valuteconverter/StartActivity.java
+++ app/src/main/java/com/cherkasik/ilya/valuteconverter/StartActivity.java
@@ -1,31 +1,102 @@
-package com.cherkasik.ilya.valuteconverter;
+ package com.cherkasik.ilya.valuteconverter;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.DatePicker;
-import android.widget.TextView;
+import android.widget.*;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class StartActivity extends AppCompatActivity {
+  private SQLiteDatabase db = create_db();
+  private Spinner spinnerFrom = findViewById(R.id.spinnerFrom);
+  private Spinner spinnerTo = findViewById(R.id.spinnerTo);
+  private RequestQueue mQueue;
 
+  private void error(String name){
+    Toast toast = Toast.makeText(getApplicationContext(), "Something wrong with " + name, Toast.LENGTH_SHORT);
+    toast.show();
+  }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_start);
-    // TODO: addItemOnSpinner(id_spinner, array items)
-    // TODO: listenCalendarDate()
-    // TODO: Convert(date, val1, val2): return res
+    getValutes();
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.menu_start, menu);
     return true;
+  }
+
+  //TODO: move to DAO
+  private SQLiteDatabase create_db(){
+    SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+    db.execSQL("CREATE TABLE IF NOT EXISTS history (conv_from TEXT, conv_to TEXT, num FLOAT, res FLOAT, date DATE)");
+    return db;
+  }
+
+  //TODO: move to DAO
+  public void add_to_history(SQLiteDatabase db, String conv_from, String conv_to, float num, float res, String date){
+    //TODO: check if there are 10 items already
+    db.execSQL("INSERT INTO history VALUES ("+ conv_from + ", " + conv_to + ", " + num + ", " + res + ", " + date);
+  }
+
+  private void addItemsOnSpinner(List<String> list){
+    ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+    dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinnerFrom.setAdapter(dataAdapter);
+    spinnerTo.setAdapter(dataAdapter);
+  }
+
+  private void getValutes() {
+    mQueue = Volley.newRequestQueue(this);
+    String url = "https://www.cbr-xml-daily.ru/daily_json.js";
+    JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+      @Override
+      public void onResponse(JSONObject response) {
+        try {
+          List<String> list = new ArrayList<>();
+          JSONArray jsonArray = response.getJSONArray("Valute");
+          for (int i = 0; i < jsonArray.length(); i++){
+            JSONObject valute = jsonArray.getJSONObject(i);
+            list.add(valute.getString("Name"));
+          }
+          addItemsOnSpinner(list);
+
+        } catch (JSONException e) {
+          error("json response");
+        }
+      }
+    }, new Response.ErrorListener() {
+      @Override
+      public void onErrorResponse(VolleyError e) {
+        error("request");
+      }
+    });
+    mQueue.add(request);
   }
 
   public void onSettingsMenuClicked(MenuItem item) {
@@ -44,5 +115,7 @@ public class StartActivity extends AppCompatActivity {
   public void convert(View view) {
     TextView textView = findViewById(R.id.result);
     textView.setText(getDate());
+    //TODO: Convert(date, val1, val2): return res
+    //TODO: add_to_history(db, ...);
   }
 }
